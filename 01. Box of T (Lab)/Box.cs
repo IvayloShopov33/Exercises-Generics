cf67b7e0f81ad12349207b9b98b9a3850284c415
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-18 00:49:01 +0300

    Add files via upload

---
+++ 01. Box of T (Lab)/Box.cs
@@ -0,0 +1,32 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            this.items = new List<T>();
+        }
+
+        public int Count => this.items.Count;
+
+        public void Add(T item)
+        {
+            this.items.Add(item);
+        }
+
+        public T Remove()
+        {
+            T item = this.items[this.Count - 1];
+            this.items.RemoveAt(this.Count - 1);
+
+            return item;
+        }
+
+        public T[] ToArray()
+        {
+            return this.items.ToArray();
+        }
+    }
+}
---
+++ 01. Box of T (Lab)/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove()); //3
+
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove()); //5
+
+            Console.WriteLine(box.Count); //3
+            Console.WriteLine(string.Join(" ", box.ToArray())); //1 2 4
+        }
+    }
+}
