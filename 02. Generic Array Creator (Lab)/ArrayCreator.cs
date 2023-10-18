8471e7ec0d4e7322629bfa22718354fd60f98a91
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-19 00:43:50 +0300

    Add files via upload

---
+++ 02. Generic Array Creator (Lab)/ArrayCreator.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] values = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                values[i] = item;
+            }
+
+            return values;
+        }
+    }
+}
---
+++ 02. Generic Array Creator (Lab)/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] names = ArrayCreator.Create<string>(5, "John");
+            int[] numbers = ArrayCreator.Create<int>(8, 15);
+            Console.WriteLine(string.Join(", ", names));
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
