f71ee587a3a8e2cb3174ff192b6363f5631e3713
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-20 00:31:48 +0300

    Add files via upload

---
+++ 03. Generic Scale (Lab)/EqualityScale.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T leftItem;
+        private T rightItem;
+
+        public EqualityScale(T leftItem, T rightItem)
+        {
+            this.leftItem = leftItem;
+            this.rightItem = rightItem;
+        }
+
+        public bool AreEqual()
+        {
+            return this.leftItem.Equals(this.rightItem);
+        }
+    }
+}
---
+++ 03. Generic Scale (Lab)/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>()
+            {
+                "Ivaylo",
+                "Shopov"
+            };
+
+            List<string> list2 = new List<string>()
+            {
+                "Ivaylo",
+                "Shopov"
+            };
+
+            EqualityScale<List<string>> equalityScale = new EqualityScale<List<string>>(list, list2);
+            Console.WriteLine(equalityScale.AreEqual()); //False
+
+            List<string> list3 = list2;
+            var equalityScale1 = new EqualityScale<List<string>>(list3, list2);
+            Console.WriteLine(equalityScale1.AreEqual()); //True
+
+            string text = "Some random text";
+            string text1 = "Some random text";
+            var equalityScale2 = new EqualityScale<string>(text, text1);
+            Console.WriteLine(equalityScale2.AreEqual()); //True
+
+            int a = 5;
+            int b = 3;
+            var equalityScale3 = new EqualityScale<int>(a, b);
+            Console.WriteLine(equalityScale3.AreEqual()); //False
+        }
+    }
+}
