7b26fea3dfa2cd8bf2c526ec25f50e1809d7c505
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-21 00:03:33 +0300

    Add files via upload

---
+++ 01. Generic Box of String/Box.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Generic_Box_of_String
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T input)
+        {
+            this.item = input;
+        }
+
+        public override string ToString()
+        {
+            Type type = this.item.GetType();
+            string output = $"{type}: {this.item}";
+
+            return output;
+        }
+    }
+}
---
+++ 01. Generic Box of String/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01._Generic_Box_of_String
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            int stringsCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= stringsCount; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                boxes.Add(box);
+            }
+
+            foreach (var item in boxes)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
