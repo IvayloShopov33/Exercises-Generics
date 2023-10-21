71d3d71197945583f59701ccbdf06333e50bf9d5
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-22 00:15:07 +0300

    Add files via upload

---
+++ 02. Generic Box of Integer/Box.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Generic_Box_of_Integer
+{
+    public class Box<T>
+    {
+        private T element;
+
+        public Box(T element)
+        {
+            this.element = element;
+        }
+
+        public override string ToString()
+        {
+            Type type = typeof(T);
+            return $"{type}: {this.element}";
+        }
+    }
+}
---
+++ 02. Generic Box of Integer/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02._Generic_Box_of_Integer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box<int>> boxesOfIntegers = new List<Box<int>>();
+
+            int numbersCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(number);
+                boxesOfIntegers.Add(box);
+            }
+
+            foreach (Box<int> item in boxesOfIntegers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
