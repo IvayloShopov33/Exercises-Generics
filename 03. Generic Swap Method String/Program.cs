eb305241e53e5f73327c534dacf19d1264dbfda1
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-23 00:16:22 +0300

    Add files via upload

---
+++ 03. Generic Swap Method String/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _03._Generic_Swap_Method_String
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string>();
+
+            int itemsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < itemsCount; i++)
+            {
+                string value = Console.ReadLine();
+                strings.Add(value);
+            }
+
+            int[] indexes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            if (IsInside(strings, firstIndex, secondIndex))
+            {
+                SwapElementsAndPrintThem<string>(strings, firstIndex, secondIndex);
+            }
+        }
+
+        public static void SwapElementsAndPrintThem<T>(List<T> values, int firstIndex, int secondIndex)
+        {
+            T tempValue = values[firstIndex];
+            values[firstIndex] = values[secondIndex];
+            values[secondIndex] = tempValue;
+
+            foreach (T value in values)
+            {
+                Type type = value.GetType();
+                Console.WriteLine($"{type}: {value}");
+            }
+        }
+
+        public static bool IsInside(List<string> list, int firstIndex, int secondIndex)
+        {
+            return firstIndex >= 0 && firstIndex < list.Count &&
+                secondIndex >= 0 && secondIndex < list.Count;
+        }
+    }
+}
