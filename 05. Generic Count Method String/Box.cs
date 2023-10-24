4baeaef6a5191bde86c7198b471d086bd7cd22c8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-25 00:08:42 +0300

    Add files via upload

---
+++ 05. Generic Count Method String/Box.cs
@@ -0,0 +1,27 @@
+﻿namespace _05._Generic_Count_Method_String
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        private List<T> items;
+
+        public Box(List<T> input)
+        {
+            this.items = input;
+        }
+
+        public int CalculateCountOfGreaterElements(T element)
+        {
+            int countOfGreaterElements = 0;
+            foreach (T item in this.items)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    countOfGreaterElements++;
+                }
+            }
+
+            return countOfGreaterElements;
+        }
+    }
+}
---
+++ 05. Generic Count Method String/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05._Generic_Count_Method_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string>();
+            Box<string> box = new Box<string>(strings);
+
+            int itemsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < itemsCount; i++)
+            {
+                string input = Console.ReadLine();
+                strings.Add(input);
+            }
+
+            string itemToCompare = Console.ReadLine();
+            int result = box.CalculateCountOfGreaterElements(itemToCompare);
+            Console.WriteLine(result);
+        }
+    }
+}
