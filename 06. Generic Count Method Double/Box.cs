2deda23da89ad725a4f6753d9054ece9ea691581
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-25 00:09:32 +0300

    Add files via upload

---
+++ 06. Generic Count Method Double/Box.cs
@@ -0,0 +1,27 @@
+﻿namespace _06._Generic_Count_Method_Double
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        private List<T> numbers;
+
+        public Box(List<T> values)
+        {
+            this.numbers = values;
+        }
+
+        public int CalculateCountOfGreaterValues(T item)
+        {
+            int countOfGreaterValues = 0;
+            foreach (T element in this.numbers)
+            {
+                if (element.CompareTo(item) > 0)
+                {
+                    countOfGreaterValues++;
+                }
+            }
+
+            return countOfGreaterValues;
+        }
+    }
+}
---
+++ 06. Generic Count Method Double/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _06._Generic_Count_Method_Double
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = new List<double>();
+            Box<double> box = new Box<double>(numbers);
+
+            int countOfNumbers = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                numbers.Add(number);
+            }
+
+            double numberToCompare = double.Parse(Console.ReadLine());
+            int result = box.CalculateCountOfGreaterValues(numberToCompare);
+            Console.WriteLine(result);
+        }
+    }
+}
