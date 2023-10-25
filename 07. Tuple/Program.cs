dbabc326b05487237c91e15920086b91eaa65817
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-26 01:31:51 +0300

    Add files via upload

---
+++ 07. Tuple/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _07._Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstLine = Console.ReadLine();
+            string secondLine = Console.ReadLine();
+            string thirdLine = Console.ReadLine();
+
+            string[] firstLineDetails = firstLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] secondLineDetails = secondLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] thirdLineDetails = thirdLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string firstAndLastName = firstLineDetails[0] + " " + firstLineDetails[1];
+            string address = firstLineDetails[2];
+            MyTuple<string, string> nameWithAddress = new MyTuple<string, string>(firstAndLastName, address);
+
+            string secondLineName = secondLineDetails[0];
+            int litersOfBeer = int.Parse(secondLineDetails[1]);
+            MyTuple<string, int> nameWithLitersOfBeer = new MyTuple<string, int>(secondLineName, litersOfBeer);
+
+            int firstNumber = int.Parse(thirdLineDetails[0]);
+            double secondNumber = double.Parse(thirdLineDetails[1]);
+            MyTuple<int, double> numbers = new MyTuple<int, double>(firstNumber, secondNumber);
+
+            nameWithAddress.PrintOutput();
+            nameWithLitersOfBeer.PrintOutput();
+            numbers.PrintOutput();
+        }
+    }
+}
---
+++ 07. Tuple/Tuple.cs
@@ -0,0 +1,20 @@
+﻿namespace _07._Tuple
+{
+    public class MyTuple<T1, T2>
+    {
+        public MyTuple(T1 firstItem, T2 secondItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+        }
+
+        public T1 FirstItem { get; set; }
+
+        public T2 SecondItem { get; set; }
+
+        public void PrintOutput()
+        {
+            Console.WriteLine($"{this.FirstItem} -> {this.SecondItem}");
+        }
+    }
+}
