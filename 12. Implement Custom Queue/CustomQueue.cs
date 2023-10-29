bce57830062d8d41fc7af8f1fc8eafc13be4d94f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-30 01:47:59 +0200

    Add files via upload

---
+++ 12. Implement Custom Queue/CustomQueue.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+
+namespace _12._Implement_Custom_Queue
+{
+    internal class CustomQueue<T> : IEnumerable<T>
+    {
+        private const int InitialCapacity = 4;
+        private T[] items;
+
+        public CustomQueue()
+        {
+            this.items = new T[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Enqueue(T element)
+        {
+            if (this.Count == this.items.Length)
+            {
+                Resize();
+            }
+
+            this.items[this.Count++] = element;
+        }
+
+        public T Dequeue()
+        {
+            IsQueueEmpty();
+
+            T element = this.items[0];
+            if (this.Count <= this.items.Length / 4)
+            {
+                Shrink();
+            }
+
+            this.Count--;
+            Shift();
+
+            return element;
+        }
+
+        public T Peek()
+        {
+            IsQueueEmpty();
+
+            T element = this.items[0];
+
+            return element;
+        }
+
+        public void Clear()
+        {
+            this.Count = 0;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in this.items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private void IsQueueEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+        }
+
+        private void Shrink()
+        {
+            T[] tempArray = new T[this.items.Length / 2];
+            for (int i = 0; i < this.Count; i++)
+            {
+                tempArray[i] = this.items[i];
+            }
+
+            this.items = tempArray;
+        }
+
+        private void Shift()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+        }
+
+        private void Resize()
+        {
+            T[] tempArray = new T[this.items.Length * 2];
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                tempArray[i] = this.items[i];
+            }
+
+            this.items = tempArray;
+        }
+    }
+}
---
+++ 12. Implement Custom Queue/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _12._Implement_Custom_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue<int> customQueue = new CustomQueue<int>();
+            customQueue.Enqueue(1);
+            customQueue.Enqueue(2);
+            customQueue.Clear();
+            customQueue.Enqueue(3);
+            Console.WriteLine(customQueue.Peek());
+            customQueue.Enqueue(4);
+            customQueue.Enqueue(5);
+            customQueue.Dequeue();
+            customQueue.Enqueue(6);
+            customQueue.ForEach(Console.WriteLine);
+
+            CustomQueue<string> queue = new CustomQueue<string>();
+            queue.Enqueue("hello");
+            queue.Enqueue("world");
+            queue.Clear();
+            queue.Enqueue("Hello!");
+            queue.Enqueue("Hi!");
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
